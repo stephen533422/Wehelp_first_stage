7cd14d14f612be12a4b8bc653d9ce18d122b1dbd
star533422 <44090848+stephen533422@users.noreply.github.com>
2023-08-09 22:36:57 +0800

    Add files via upload

---
+++ week6/static/home.js
@@ -0,0 +1,23 @@
+function checkForm(){
+    console.log("Check");
+    let name = document.getElementById("signupName");
+    let username = document.getElementById("signupUsername");
+    let password = document.getElementById("signupPassword");
+    console.log(name.value, username.value, password.value);
+    if(name.value===""){
+        alert("請輸入姓名");
+        return false;
+    }
+    else if(username.value===""){
+        alert("請輸入帳號");
+        return false;
+    }
+    else if(password.value===""){
+        alert("請輸入密碼");
+        return false;
+    }
+    else{
+        return true;
+    }
+}
+
